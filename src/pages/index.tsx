63e4a679a41dd36b6e0a1ba209eb14798b56f318
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-06-19 19:09:56 -0300

    Banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Flex } from "@chakra-ui/react";
-import Header from "./components/header";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
 
 
 export default function Home() {
   return (
-    <Flex dir="column">
+    <Flex direction="column">
       <Header />
+      <Banner />
     </Flex>
   )
 }
